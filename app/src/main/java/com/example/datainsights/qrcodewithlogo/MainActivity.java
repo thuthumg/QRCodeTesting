1dde9dd87066af82b1dc89f30a58ee6d79614ea7
THUTHUMAUNG <ms.jellyfishgirl@gmail.com>
2019-01-13 23:56:29 -0800

    send email with QR code

--- app/src/main/java/com/example/datainsights/qrcodewithlogo/MainActivity.java
+++ app/src/main/java/com/example/datainsights/qrcodewithlogo/MainActivity.java
@@ -2,17 +2,24 @@ package com.example.datainsights.qrcodewithlogo;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.Environment;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -25,6 +32,10 @@ import com.google.zxing.Result;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -37,38 +48,43 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
     EditText editText;
     TextView textView;
     private ZXingScannerView zXingScannerView;
+    private static final int PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 25;
+    private static final String IMAGE_DIRECTORY = "/QRcodeDemonuts";
+    Button btn_mail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageViewBitmap= findViewById(R.id.imageViewBitmap);
+        imageViewBitmap = findViewById(R.id.imageViewBitmap);
         textView = findViewById(R.id.showTxt);
-
+        btn_mail = findViewById(R.id.btn_mail);
+        btn_mail.setVisibility(View.GONE);
     }
     //handling button click
 
 
-    public void GenerateClick(View view){
+    public void GenerateClick(View view) {
         try {
             //setting size of qr code
-            int width =400;
+            int width = 400;
             int height = 400;
             int smallestDimension = width < height ? width : height;
 
-             editText=findViewById(R.id.editText) ;
+            editText = findViewById(R.id.editText);
             String qrCodeData = editText.getText().toString();
             //setting parameters for qr code
             String charset = "UTF-8";
-            Map<EncodeHintType, ErrorCorrectionLevel> hintMap =new HashMap<EncodeHintType, ErrorCorrectionLevel>();
+            Map<EncodeHintType, ErrorCorrectionLevel> hintMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
             hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
             CreateQRCode(qrCodeData, charset, hintMap, smallestDimension, smallestDimension);
 
         } catch (Exception ex) {
-            Log.e("QrGenerate",ex.getMessage());
+            Log.e("QrGenerate", ex.getMessage());
         }
     }
 
-    public  void CreateQRCode(String qrCodeData, String charset, Map hintMap, int qrCodeheight, int qrCodewidth){
+    public void CreateQRCode(String qrCodeData, String charset, Map hintMap, int qrCodeheight, int qrCodewidth) {
 
 
         try {
@@ -86,7 +102,7 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
                 for (int x = 0; x < width; x++) {
                     //pixels[offset + x] = matrix.get(x, y) ? BLACK : WHITE;
                     pixels[offset + x] = matrix.get(x, y) ?
-                            ResourcesCompat.getColor(getResources(),R.color.colorPrimary,null) :WHITE;
+                            ResourcesCompat.getColor(getResources(), R.color.colorPrimary, null) : WHITE;
                 }
             }
 
@@ -94,16 +110,55 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
             bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
             //setting bitmap to image view
 
-            Bitmap overlay = BitmapFactory.decodeResource(getResources(),R.drawable.sample);
+            Bitmap overlay = BitmapFactory.decodeResource(getResources(), R.drawable.sample);
+            Bitmap mergeBit = mergeBitmaps(overlay, bitmap);
 
-            imageViewBitmap.setImageBitmap(mergeBitmaps(overlay,bitmap));
+            imageViewBitmap.setImageBitmap(mergeBitmaps(overlay, bitmap));
 
-        }catch (Exception er){
-            Log.e("QrGenerate",er.getMessage());
+
+            // String path = saveImage(bitmap);  //give read write permission
+            String path = saveImage(mergeBit);
+            Toast.makeText(MainActivity.this, "QRCode saved to -> " + path + ", merge = " + mergeBitmaps(overlay, bitmap), Toast.LENGTH_SHORT).show();
+            btn_mail.setVisibility(View.VISIBLE);
+
+
+        } catch (Exception er) {
+            Log.e("QrGenerate", er.getMessage());
         }
     }
 
+    public String saveImage(Bitmap myBitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File wallpaperDirectory = new File(
+                Environment.getExternalStorageDirectory() + IMAGE_DIRECTORY);
+        // have the object build the directory structure, if needed.
 
+        if (!wallpaperDirectory.exists()) {
+            Log.d("dirrrrrr", "" + wallpaperDirectory.mkdirs());
+            wallpaperDirectory.mkdirs();
+        }
+
+        try {
+            /*File f = new File(wallpaperDirectory, Calendar.getInstance()
+                    .getTimeInMillis() + ".jpg");*/
+            File f = new File(wallpaperDirectory, "RandomQR.jpg");
+            f.createNewFile();   //give read write permission
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+            MediaScannerConnection.scanFile(this,
+                    new String[]{f.getPath()},
+                    new String[]{"image/jpeg"}, null);
+            fo.close();
+            Log.d("TAG", "File Saved::--->" + f.getAbsolutePath());
+
+            return f.getAbsolutePath();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return "";
+
+    }
 
     public Bitmap mergeBitmaps(Bitmap overlay, Bitmap bitmap) {
 
@@ -117,13 +172,14 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
 
         canvas.drawBitmap(bitmap, new Matrix(), null);
 
-        int centreX = (canvasWidth  - overlay.getWidth()) /2;
-        int centreY = (canvasHeight - overlay.getHeight()) /2 ;
+        int centreX = (canvasWidth - overlay.getWidth()) / 2;
+        int centreY = (canvasHeight - overlay.getHeight()) / 2;
         canvas.drawBitmap(overlay, centreX, centreY, null);
 
         return combined;
     }
-    public void scanfunc(View view){
+
+    public void scanfunc(View view) {
         zXingScannerView = new ZXingScannerView(getApplicationContext());
         setContentView(zXingScannerView);
         zXingScannerView.setResultHandler(this);
@@ -134,10 +190,12 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
     @Override
     protected void onPause() {
         super.onPause();
+        zXingScannerView = new ZXingScannerView(getApplicationContext());
+
         zXingScannerView.stopCamera();
     }
 
-    public void scanQRfunc(View view){
+/*    public void scanQRfunc(View view) {
 
 
         try {
@@ -150,11 +208,12 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         } catch (Exception e) {
 
             Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
-            Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
+            Intent marketIntent = new Intent(Intent.ACTION_VIEW, marketUri);
             startActivity(marketIntent);
 
         }
-    }
+    }*/
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -165,7 +224,7 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
                 textView.setText(contents);
 
             }
-            if(resultCode == RESULT_CANCELED){
+            if (resultCode == RESULT_CANCELED) {
                 //handle cancel
             }
         }
@@ -173,8 +232,43 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
 
     @Override
     public void handleResult(Result result) {
-        Toast.makeText(getApplicationContext(),result.getText(),Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), result.getText(), Toast.LENGTH_SHORT).show();
         zXingScannerView.resumeCameraPreview(this);
-       // Intent intent = new Intent(this,)
+        // Intent intent = new Intent(this,)
+    }
+
+    public void sendMail(View view) {
+
+
+        if (!isPermissionGranted(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+        } else {
+            sendMail2();
+        }
+
+
+    }
+
+    private boolean isPermissionGranted(String permission) {
+        return ContextCompat.checkSelfPermission(getApplicationContext(),
+                permission)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public void sendMail2() {
+
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("image/*");
+        String imagePath = Environment.getExternalStorageDirectory()
+                + IMAGE_DIRECTORY + "/RandomQR.jpg";
+        File imageFileToShare = new File(imagePath);
+        Uri uri = FileProvider.getUriForFile(MainActivity.this, BuildConfig.APPLICATION_ID + ".provider", imageFileToShare);
+        Log.d("sendMailfunc", "file path = " + imageFileToShare + ",  " + uri);
+        share.putExtra(Intent.EXTRA_STREAM, uri);
+        share.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        share.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        startActivity(Intent.createChooser(share, "Share Image!"));
     }
 }
